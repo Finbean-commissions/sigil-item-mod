42ee23f1514ebdd70a21a66408d51eb4b3122f82
Finbean's <finbean.commissions@gmail.com>
2023-09-13 16:23:07 +0100

    ok

--- main.lua
+++ main.lua
@@ -1,11 +1,36 @@
 ----Welcome to the "main.lua" file! Here is where all the magic happens, everything from functions to callbacks are done here.
 --Startup
-local mod = RegisterMod("Ars Goetla", 1)
+local mod = RegisterMod("Commission Template - Items (Passive, Active, Trinket and Card)", 1)
 local game = Game()
 
 mod.Items = {
-	Lucifer = Isaac.GetItemIdByName("Lucifer Sigil"),
+    Passive = Isaac.GetItemIdByName("Passive Example"),
+    Active = Isaac.GetItemIdByName("Active Example"),
+    Trinket = Isaac.GetTrinketIdByName("Trinket Example"),
+    Card = Isaac.GetCardIdByName("Card Example"),
 }
+
+function mod:UseItem(item, _, player, UseFlags, Slot, _)
+	if UseFlags & UseFlag.USE_OWNED == UseFlag.USE_OWNED then
+		if item == mod.Items.Active then
+            player:AnimateCollectible(mod.Items.Active, "UseItem")
+            game:Fart(player.Position, 85, nil, 1, 0, Color.Default)
+            for i = 1,2 do
+                Isaac.Spawn(5, 300, mod.Items.Card, player.Position, Vector(0,-48), nil)
+            end
+        end
+    end
+end
+mod:AddCallback(ModCallbacks.MC_USE_ITEM, mod.UseItem)
+
+function mod:UseCard(card, player)
+    if card == mod.Items.Card then
+        for i = 1,8 do
+            Isaac.Spawn(5, 10, 0, player.Position, RandomVector(), nil)
+        end
+    end
+end
+mod:AddCallback(ModCallbacks.MC_USE_CARD,mod.UseCard)
 
 local function toTears(fireDelay) --thanks oat for the cool functions for calculating firerate
 	return 30 / (fireDelay + 1)
@@ -13,16 +38,19 @@ end
 local function fromTears(tears)
 	return math.max((30 / tears) - 1, -0.99)
 end
-
 function mod:CacheEvaluation(player, cacheFlag)
-	if player:HasCollectible(mod.Items.Lucifer) == true then
+	if player:HasCollectible(mod.Items.Passive) == true then
+		if cacheFlag == CacheFlag.CACHE_FIREDELAY then
+			player.MaxFireDelay = math.max(1.0, fromTears(toTears(player.MaxFireDelay) + 1.22 * player:GetCollectibleNum(mod.Items.Passive, true)))
+		end
+	end
+	if player:HasTrinket(mod.Items.Trinket) == true then
+		if cacheFlag == CacheFlag.CACHE_SPEED then
+			player.MoveSpeed = player.MoveSpeed - 0.2 * player:GetTrinketMultiplier(mod.Items.Trinket)
+		end
 		if cacheFlag == CacheFlag.CACHE_DAMAGE then
-			player.Damage = player.Damage + 1.50 * player:GetCollectibleNum(mod.Items.Lucifer, true)
+			player.Damage = player.Damage + 2 * player:GetTrinketMultiplier(mod.Items.Trinket)
 		end
-		if cacheFlag == CacheFlag.CACHE_FLYING then
-			player.CanFly = true
-		end
-		print("got")
 	end
 end
 mod:AddCallback(ModCallbacks.MC_EVALUATE_CACHE,mod.CacheEvaluation)
